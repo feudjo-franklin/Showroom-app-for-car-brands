e7a851357db3aa391cd8e0c4ec07e0499bc4ff32
Franklin <franklinfeudjo1@gmail.com>
2021-12-12 14:59:53 +0100

    Implemented WebApplicationException for a better machine level consumption

--- src/main/java/org/studyeasy/showroom/resources/Demo.java
+++ src/main/java/org/studyeasy/showroom/resources/Demo.java
@@ -1,14 +1,14 @@
 package org.studyeasy.showroom.resources;
 
 import javax.servlet.ServletContext;
-import javax.ws.rs.CookieParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
 @Path("/")
@@ -21,7 +21,25 @@ public class Demo {
 	@Context
 	private ServletContext servletContext;
 	
+	@GET
+	public String usefulAnnotations() {
+		
+		return "Hello Jax-rs";
+	}
 	
+	@GET
+	@Path("demo")
+	public String contextDemo(@HeaderParam("header") String header) throws Exception  {
+		Response response = Response.status(404).entity("The value of custom header not found").build();
+		if(header == null) {
+			throw new WebApplicationException(response);
+		}
+		return "The value of custom header value is : " + header;
+	}
+	
+	//****************** OLD CODE IMPLEMENTATION *******************
+	
+	/*
 	@GET
 	public String usefulAnnotations(@HeaderParam("HeaderValue") String headerAttrib,
 			@CookieParam("_xsrf") String xsrf) {
@@ -29,7 +47,10 @@ public class Demo {
 		return "Hello Jax-rx";
 		//return "Header Value: " + headerAttrib + " Cookie _xsrf value: " + xsrf;
 	}
+	*/
 	
+	
+	/*
 	@GET
 	@Path("demo")
 	public String contextDemo(@Context HttpHeaders header) {
@@ -38,6 +59,7 @@ public class Demo {
 		return header.getRequestHeaders().keySet().toString();
 		//return "Absolute Path: " + uriInfo.getAbsolutePath().toString();
 	}
+	*/
 	
 	
 	
