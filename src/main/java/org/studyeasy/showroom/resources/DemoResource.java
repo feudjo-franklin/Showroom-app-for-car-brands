4bbfd7f90f4f9c78b076ed3b5486b933bf875fae
Franklin <franklinfeudjo1@gmail.com>
2021-12-14 07:12:25 +0100

    Added Exception mapper functionality to include a json response

--- src/main/java/org/studyeasy/showroom/resources/DemoResource.java
+++ src/main/java/org/studyeasy/showroom/resources/DemoResource.java
@@ -3,15 +3,13 @@ package org.studyeasy.showroom.resources;
 import javax.servlet.ServletContext;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
-import org.studyeasy.showroom.model.ErrorPayload;
 
 @Path("/")
 public class DemoResource {
@@ -33,10 +31,8 @@ public class DemoResource {
 	@Path("demo")
 	@Produces({MediaType.TEXT_PLAIN, MediaType.APPLICATION_JSON})  // {} curly braces to specify a list of the types of response the method will retuq 
 	public String contextDemo(@HeaderParam("header") String header) throws Exception  {
-		ErrorPayload error = new ErrorPayload(404, "The value of custom header not found");
-		Response response = Response.status(404).entity(error).build();
 		if(header == null) {
-			throw new WebApplicationException(response);
+			throw new NotFoundException();
 		}
 		return "The value of custom header value is : " + header;
 	}
